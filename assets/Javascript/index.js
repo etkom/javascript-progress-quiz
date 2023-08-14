766554702c3ce0238020da80da2b7382f018256c
etkom <136800424+etkom@users.noreply.github.com>
2023-08-14 09:46:37 +0100

    declare global scope variables and add questions to readme

---
+++ assets/Javascript/index.js
@@ -0,0 +1,8 @@
+// Declare global scope variables
+const startButton = document.querySelector("#start");
+const timeDisplay = document.querySelector("#time");
+const questionTitle = document.querySelector("#question-title");
+const choicesContainer = document.querySelector("#choices");
+const feedbackContainer = document.querySelector("#feedback");
+const initialsInput = document.querySelector("#initials");
+const submitButton = document.querySelector("#submit");
